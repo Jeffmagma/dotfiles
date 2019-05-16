201d47598f50382d436738b3bacb4a6eaeb0acf3
Jeffrey Gao <jeffrey.magmachu@gmail.com>
2019-05-16 18:13:58 -0400

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# update and upgrade packages
+sudo apt update
+sudo apt upgrade
+
+# add stuff to bashrc
+
+# add stuff to vimrc
+sudo apt install vim
+cp .vimrc ~/.vimrc
+
+# install i3-gaps
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake libxcb-xrm-dev
+cd /tmp
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+autoreconf --force --install
+rm -rf build
+mkdir build
+cd build
+../configure --prefix=/usr --sysconfdir=/etc
+make
+sudo make install
+
+# configure i3
+"gaps inner 10" >> 
