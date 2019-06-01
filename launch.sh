b1ec9c2dbb0aabc542b4ca7297f08261b5056ac7
Jeffrey Gao <jeffrey.magmachu@gmail.com>
2019-06-01 17:19:24 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -26,8 +26,13 @@ make
 sudo make install
 
 # install powerline
+sudo apt install powerline
 
-# install polybar
+# install and configure polybar
+sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
+git clone https://github.com/jaagr/polybar.git
+cd polybar && ./build.sh
+cp polybar-config ~/.config/polybar/config
 
 # configure i3
-"gaps inner 10" >> 
+cp i3-config ~/.config/i3/config
